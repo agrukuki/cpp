3a5f97711b4460b3cc768ab0103ee7cd9b0c8cde
agrukuki <agru285@gmail.com>
2020-03-07 09:24:55 +0700

    Add files via upload

--- B. T-primes.cpp
+++ B. T-primes.cpp
@@ -1,35 +1,39 @@
 #include <bits/stdc++.h>
 #define ll long long
-#define maxN 100000
+#define maxN 1000009
+// sqrt 10^12 = 10^6
 using namespace std;
-ll n,a[maxN];
-bool TP(ll x) {
-    ll cnt(0);
-    for (int i = 2;i < x;i++) {
-        if (x % i == 0) {
-            cnt++;
+ll n,x;
+bool Eratos[maxN];
+void Init() {
+    Eratos[0] = false;
+    Eratos[1] = false;
+    for (int i = 2;i <= maxN;i++) {
+        Eratos[i] = true;
+    }
+    ll smn = sqrt(maxN);
+    for (int i = 2;i <= smn;i++) {
+        if (Eratos[i] == true) {
+            for (int j = i*i;j <= maxN;j+=i) {
+                Eratos[j] = false;
+            }
         }
-        if (cnt > 1) return false;
     }
-    if (cnt == 0) {
-        return false;
-    }
-    //cout << cnt << endl;
-    return true;
 }
 void Enter() {
-    cin >> n;
-    for (int i = 0;i < n;i++)
-        cin >> a[i];
+    cin >> x;
 }
 void Solve() {
-    for (int i = 0;i < n;i++) {
-        if (TP(a[i])) cout << "YES" << endl;
-        else cout << "NO" << endl;
-    }
+    ll t = sqrt(x);
+    if (t * t == x && Eratos[t]) cout << "YES" << endl;
+    else cout << "NO" << endl;
 }
 int main() {
-    Enter();
-    Solve();
+    Init();
+    cin >> n;
+    for (int i = 0;i < n;i++) {
+        Enter();
+        Solve();
+    }
 }
 
